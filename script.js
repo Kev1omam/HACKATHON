1d4d9bf5998d3f59725e0db30a26fcba69c3ed75
THISmann <fuhetienne0@gmail.com>
2022-05-06 23:12:19 +0100

    save

--- script.js
+++ script.js
@@ -1,69 +1,74 @@
-let color ; 
-let tab = [];
-let cameroun = ["green", "red" , "yellow" , "green" , "red" , "yellow"];
-let france = ["blue", "white" , "red" , "blue", "white" , "red" ];
-let senegal = ["green" , "jaune" , "rouge" ,"green" , "jaune" , "rouge"  ];
-let tchad = ["blue", "yellow" , "red" , "blue", "yellow" , "red"];
-let canada = ["red", "white" , "red" ,"red", "white" , "red"];
+///////////////////////////////////////////////////////////////////////////
+// Hackathon project theme :  Game Flag                                  //
+// write by Oman Kevin and fuh goolove etienne                           // 
+///////////////////////////////////////////////////////////////////////////
 
+// gloval variable to set the color 
+let color;
+
+// content the element that the user have selected 
+let tab = [];
+
+// cameroun position element color
+let cameroun = ["green", "red", "yellow", "green", "red", "yellow"];
+// france position element color
+let france = ["blue", "white", "red", "blue", "white", "red"];
+// senegal position element color
+let senegal = ["green", "jaune", "rouge", "green", "jaune", "rouge"];
+// tchad position element color
+let tchad = ["blue", "yellow", "red", "blue", "yellow", "red"];
+// canada position element color
+let canada = ["red", "white", "red", "red", "white", "red"];
+// variable the content the name of the country
 let country;
+
+// drop over event
 function dropOver(e) {
-    e.preventDefault(); 
-    console.log("le toucher")
+  e.preventDefault();
 }
 
+// drag event 
 function drag(e) {
-    e.dataTransfer.setData("text", e.target.id );
-    color = e.target.id;
-    console.log("le commencement" , e.target.id );
-  }
+  // set the id of the target 
+  e.dataTransfer.setData("text", e.target.id);
+  color = e.target.id;
+  console.log("le commencement", e.target.id);
+}
 
+// drop event 
 function drop(e) {
-    e.preventDefault(); 
-    var data = e.dataTransfer.getData("text");
-    e.target.append(document.getElementById(data));
-    e.target.style.backgroundColor = color;
-    console.log("finish", e.target.id , color);
-    //tab.push(color);
-    tab[e.target.id - 1] = color
-    console.log(tab);
+  e.preventDefault();
+  var data = e.dataTransfer.getData("text");
+  e.target.append(document.getElementById(data));
+  e.target.style.backgroundColor = color;
+  console.log("finish", e.target.id, color);
+  //tab.push(color);
+  tab[e.target.id - 1] = color
+  console.log(tab);
 
-    if(tab.length == 6 && tab[0] !== undefined && tab[1] != undefined && tab[2] != undefined && tab[3] != undefined && tab[4] != undefined && tab[5] != undefined){
-      console.log("ready !!!");
-      console.log(tab);
-      // for (let i = 0; i < tab.length; i++) {
-      //     if (tab[i] === cameroun[i]) {
-      //         country = "cameroun"
-      //     }
-      // }
+  if (tab.length == 6 && tab[0] !== undefined && tab[1] != undefined && tab[2] != undefined && tab[3] != undefined && tab[4] != undefined && tab[5] != undefined) {
 
-     if(JSON.stringify(tab)==JSON.stringify(cameroun)){
-        country = "cameroun"
-        alert(country);
-     } else if(JSON.stringify(tab)==JSON.stringify(france)){
+    if (JSON.stringify(tab) == JSON.stringify(cameroun)) {
+      country = "cameroun"
+      alert(country);
+    } else if (JSON.stringify(tab) == JSON.stringify(france)) {
       country = "france"
       alert(country);
-   } else if(JSON.stringify(tab)==JSON.stringify(canada)){
-    country = "canada"
-    alert(country);
- } else if(JSON.stringify(tab)==JSON.stringify(senegal)){
-  country = "senegal"
-  alert(country);
-} else if(JSON.stringify(tab)==JSON.stringify(tchad)){
-  country = "tchad"
-  alert(country);
-} else {
-  alert("try Again")
-}
-
-      
-      
+    } else if (JSON.stringify(tab) == JSON.stringify(canada)) {
+      country = "canada"
+      alert(country);
+    } else if (JSON.stringify(tab) == JSON.stringify(senegal)) {
+      country = "senegal"
+      alert(country);
+    } else if (JSON.stringify(tab) == JSON.stringify(tchad)) {
+      country = "tchad"
+      alert(country);
+    } else {
+      alert("try Again")
     }
 
-
-   
   }
 
-  
+}
 
-console.log("bonjour");
+
